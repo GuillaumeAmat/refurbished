ad5aba36da6fc69d7abee498d0af240aa1b120ed
Guillaume AMAT <guillaume@amat.io>
2026-02-19 22:58:13 +0100

    fixup! feat: speed up the normal & dash speed

--- game/constants.ts
+++ game/constants.ts
@@ -24,10 +24,10 @@ export const BACKGROUND_COLOR_G = '141';
 export const BACKGROUND_COLOR_B = '142';
 
 export const TILE_SIZE = 2;
-export const MOVEMENT_SPEED = 20;
-export const DASH_SPEED = 60;
+export const MOVEMENT_SPEED = 15;
+export const DASH_SPEED = 40;
 export const DASH_DURATION = 0.2;
-export const DASH_COOLDOWN = 0.4;
+export const DASH_COOLDOWN = 0.3;
 
 export const INTERACTION_DISTANCE = 1.4;
 export const INTERACTION_FACING_THRESHOLD = 0.5;
