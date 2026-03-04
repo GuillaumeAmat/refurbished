279749436fbaacb48146ea3ec07eb02e2cd5d0dd
Guillaume AMAT <guillaume@amat.io>
2026-03-04 21:00:32 +0100

    feat: set the new color palette

--- game/constants.ts
+++ game/constants.ts
@@ -1,27 +1,18 @@
-export const MOODS = {
-  'mindaro-94': { value: '#e3f77e' },
-  'dark-bush-90': { value: '#77f8de' },
-  'light-bush-30': { value: '#005247' },
-  'dark-coffee-75': { value: '#fd9e9b' },
-  'light-coffee-35': { value: '#8d3537' },
-  'dark-tangaroa-70': { value: '#91a7fd' },
-  'light-tangaroa-20': { value: '#0a2876' },
-  'dark-violet-80': { value: '#e6b6fc' },
-  'light-violet-30': { value: '#62307e' },
-  'dark-blush-70': { value: '#fe86b2' },
-  'light-blush-50': { value: '#bd517b' },
-  'dark-cornflower-65': { value: '#7e95fb' },
-  'light-cornflower-65': { value: '#4055bf' },
-  'dark-emerald-90': { value: '#94f5bc' },
-  'light-emerald-30': { value: '#005230' },
-  'dark-purple-80': { value: '#d9baf8' },
-  'light-purple-80': { value: '#611fa3' },
+export const COLORS = {
+  white: '#ffffff',
+  mindaro: '#e2f77e',
+  night: '#121016',
+  'lilac-2': '#F9F7FC',
+  'lilac-1': '#D9D1E5',
+  lilac: '#CDB4F2',
+  'lilac+1': '#605473',
+  'lilac+2': '#2A2433',
 };
 
-export const BACKGROUND_COLOR = '#8E8D8E';
-export const BACKGROUND_COLOR_R = '142';
-export const BACKGROUND_COLOR_G = '141';
-export const BACKGROUND_COLOR_B = '142';
+export const BACKGROUND_COLOR = COLORS.mindaro;
+export const BACKGROUND_COLOR_R = '226';
+export const BACKGROUND_COLOR_G = '247';
+export const BACKGROUND_COLOR_B = '126';
 
 export const TILE_SIZE = 2;
 export const MOVEMENT_SPEED = 10;
